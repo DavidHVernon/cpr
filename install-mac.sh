3a3b390e93c4cf68010d98ccd4216fb3985ab13e
David Vernon <davidhvernon@mac.com>
2023-04-08 17:12:33 -0600

    more install script

--- install-mac.sh
+++ install-mac.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
 cd /usr/local/bin
-curl https://github.com/DavidHVernon/cpr/blob/dd8f43daedd9ffc9547e52ae543a26576a8b84bc/release/0.1.0/cpr.zip --output cpr.zip
-
+curl https://raw.githubusercontent.com/DavidHVernon/cpr/master/release/0.1.0/cpr.zip --output cpr.zip
+unzip cpr.zip
+rm cpr.zip 
